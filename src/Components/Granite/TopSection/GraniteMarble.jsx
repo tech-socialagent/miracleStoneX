bfa4fc30b36bfcda394ef311cbe55b21b2ba5182
Pavan Kumar <pavankumarts2001@gmail.com>
2023-02-28 13:25:41 +0530

    content update

--- src/Components/Granite/TopSection/GraniteMarble.jsx
+++ src/Components/Granite/TopSection/GraniteMarble.jsx
@@ -13,10 +13,9 @@ const Granite = () => {
         <Desc>
           {
             pathname == '/granite' ?
-              'Granite is formed from slow-cooling magma, which gives it its unique mineral composition and texture. It is known for its durability, strength, and beauty, making it a popular choice for counter tops, flooring, wall cladding, and more.' :
-              'Marble is formed from slow-cooling magma, which gives it its unique mineral composition and texture. It is known for its durability, strength, and beauty, making it a popular choice for counter tops, flooring, wall cladding, and more.'
+              'Granite is a type of natural stone that is formed from the slow cooling and solidification of magma or lava. It is characterized by its granular texture and is composed primarily of quartz, feldspar, and mica. Granite is known for its durability and resistance to weathering and erosion, making it a popular choice for building materials, such as counter tops, floor tiles, and monumental stones. It comes in a variety of colors, ranging from light pinks and grays to deep blacks and greens, and its natural patterns make each piece of granite unique.' :
+              'Marbles are made from a variety of materials including natural stones like limestone, and serpentine, as well as synthetic materials like glass, ceramics, and plastic. Natural marble come in a range of colors and patterns, while synthetic marbles can be made in a variety of hues, making them ideal for decoration. They are often used as building and landscaping materials, as well as in art, and collectible items. They are durable and long-lasting, and are prized for their beauty and versatility.'
           }
-          Granite is formed from slow-cooling magma, which gives it its unique mineral composition and texture. It is known for its durability, strength, and beauty, making it a popular choice for counter tops, flooring, wall cladding, and more.
         </Desc>
       </Left>
       <Right>
